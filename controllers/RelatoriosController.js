932b3179fa890573f46bb9a409db345bf2e89740
Luan <dev.luanoliveira@gmail.com>
2023-07-17 15:35:39 -0300

    Update

--- controllers/RelatoriosController.js
+++ controllers/RelatoriosController.js
@@ -1,5 +1,11 @@
+const Chamados = require('../models/Chamados');
+
 module.exports = {
     async loadRelatorios(req, res){
-        res.render('admin/admin')
+        Chamados.findAll({
+            attributes: ['created_at']
+        }).then((chamados) =>{
+            res.render('admin/relatorios', {chamados: chamados})
+        })
     }
 }
